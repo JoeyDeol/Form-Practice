06b9a1414826db573df9d3025d2765d782c23583
Joey Deol <gurjothdeol@gmail.com>
2018-05-07 06:26:51 -0400

    Messing around with form validation and trying to get it work.

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,23 +1,54 @@
 const shopifyNewsletter = {};
 
+
+shopifyNewsletter.validateMyForm = () => {
+    $('form').on('submit', (e) => {
+        e.preventDefault();
+        console.log(e)
+        return false;
+        // if (false) {
+        //     alert("FAILED")
+        //     return false;
+        // } else {
+        //     alert("PASSED")
+        //     shopifyNewsletter.httpSimulation()
+        //     shopifyNewsletter.getUserInfo();
+        //     return true
+        // }
+    })
+}
+
+shopifyNewsletter.httpSimulation = () => {
+    console.log('SUBMITTED')
+    $('#submit-email-button').empty();
+    $('#submit-email-button').text(`Submitting ...`)
+    setTimeout("shopifyNewsletter.getUserInfo", 3000)
+}
+
+
 shopifyNewsletter.getUserInfo = () => {
     $('form').on('submit', (e) => {
         e.preventDefault();
-        const userEmail = $("input[type='email']").val();
-        const userInterest = $("select").val();
-        console.log(userEmail);
-        console.log(userInterest);
-
-        //ALTERNATE METHOD BELOW WORKS AS WELL!
-        // const userEmail = e.target[1].value
-        // const userInterest = (e.target[2].value);
-        // console.log(userEmail;
-        // console.log(userInterest);
+        if (shopifyNewsletter.validateMyForm()) {
+            const userEmail = $("input[type='email']").val();
+            const userInterest = $("select").val();
+            console.log(userEmail);
+            console.log(userInterest);
+    
+            //ALTERNATE METHOD BELOW WORKS AS WELL!
+            // const userEmail = e.target[1].value
+            // const userInterest = (e.target[2].value);
+            // console.log(userEmail;
+            // console.log(userInterest);
+        } else {
+            return false;
+        }
     })
 }
 
 shopifyNewsletter.init = function () {
     shopifyNewsletter.getUserInfo();
+    shopifyNewsletter.validateMyForm();
 }
 
 // DOCUMENT READY STARTS HERE!
