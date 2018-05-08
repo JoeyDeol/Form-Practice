f5f4f0a005750d39e50eddc42ae32eab270a9451
Joey Deol <gurjothdeol@gmail.com>
2018-05-09 03:13:05 +0700

    Form Validation completed, just need to add https simulation and final screen.

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,53 +1,43 @@
 const shopifyNewsletter = {};
 
+shopifyNewsletter.validateUserEmail = (email) => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+}
 
 shopifyNewsletter.validateMyForm = () => {
     $('form').on('submit', (e) => {
         e.preventDefault();
-        console.log(e)
-        return false;
-        // if (false) {
-        //     alert("FAILED")
-        //     return false;
-        // } else {
-        //     alert("PASSED")
-        //     shopifyNewsletter.httpSimulation()
-        //     shopifyNewsletter.getUserInfo();
-        //     return true
-        // }
-    })
-}
-
-shopifyNewsletter.httpSimulation = () => {
-    console.log('SUBMITTED')
-    $('#submit-email-button').empty();
-    $('#submit-email-button').text(`Submitting ...`)
-    setTimeout("shopifyNewsletter.getUserInfo", 3000)
-}
-
-
-shopifyNewsletter.getUserInfo = () => {
-    $('form').on('submit', (e) => {
-        e.preventDefault();
-        if (shopifyNewsletter.validateMyForm()) {
-            const userEmail = $("input[type='email']").val();
-            const userInterest = $("select").val();
-            console.log(userEmail);
-            console.log(userInterest);
-    
-            //ALTERNATE METHOD BELOW WORKS AS WELL!
-            // const userEmail = e.target[1].value
-            // const userInterest = (e.target[2].value);
-            // console.log(userEmail;
-            // console.log(userInterest);
-        } else {
+        const email = $("input[type='email']").val();
+        if (shopifyNewsletter.validateUserEmail(email) == false) {
+            $('label').addClass('invalid__email');
             return false;
+        } else {
+            shopifyNewsletter.getUserInfo();
+            shopifyNewsletter.httpSimulation()
+            return true
         }
     })
 }
 
+shopifyNewsletter.getUserInfo = () => {
+    const userEmail = $("input[type='email']").val();
+    const userInterest = $("select").val();
+    console.log(userEmail);
+    console.log(userInterest);
+
+    //ALTERNATE METHOD BELOW WORKS AS WELL!
+    // const userEmail = e.target[1].value
+    // const userInterest = (e.target[2].value);
+    // console.log(userEmail;
+    // console.log(userInterest);
+}
+
+shopifyNewsletter.httpSimulation = () => {
+    console.log('SUBMITTED')
+}
+
 shopifyNewsletter.init = function () {
-    shopifyNewsletter.getUserInfo();
     shopifyNewsletter.validateMyForm();
 }
 
